76e58f9c3d95177aaf52e91fa29589e760d513a0
Andrew Lo <andrew.lo@shopify.com>
2020-03-24 22:10:44 -0400

    search input field

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,55 +1,113 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, ActivityIndicator, View, FlatList } from 'react-native';
+import {
+  StyleSheet,
+  ActivityIndicator,
+  View,
+  FlatList,
+  TextInput,
+  SafeAreaView,
+  Text,
+} from 'react-native';
 
 import { fetchArtList } from '../../services/api';
 import ArtListImage from '../../components/ArtListImage/ArtListImage';
+import SafeViewAndroid from '../../utilities/AndroidSafeArea';
 
 export default function Home() {
   const [artList, setArtList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
+    let cancelled = false;
     async function fetchList() {
-      const list = await fetchArtList();
-      setArtList(list.artObjects);
+      setLoading(true);
+
+      const list = await fetchArtList({ search: searchTerm });
+
+      if (!cancelled) {
+        setArtList(list.artObjects);
+        setLoading(false);
+      }
     }
     fetchList();
-  }, []);
+
+    return () => {
+      cancelled = true;
+    };
+  }, [searchTerm]);
 
   const renderArtItem = ({ item }) => {
     const { id, webImage } = item;
 
+    if (!webImage) {
+      return null;
+    }
+
     return (
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-        <ArtListImage key={id} url={webImage.url} />
+      <View key={id} style={{ flex: 1, flexDirection: 'row' }}>
+        <ArtListImage url={webImage.url} />
       </View>
     );
   };
 
   const renderArtList = () => {
-    if (!artList.length) {
-      return (
-        <View style={styles.loadingContainer}>
-          <ActivityIndicator size="large" color="#b3e5fc" />
-        </View>
-      );
-    }
+    const loadingIndicator = loading && (
+      <View
+        style={{
+          paddingLeft: 10,
+          paddingRight: 10,
+        }}
+      >
+        <ActivityIndicator size="small" color="#b3e5fc" />
+      </View>
+    );
+
+    const searchField = (
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TextInput
+          style={{
+            flex: 1,
+            height: 40,
+            padding: 10,
+          }}
+          value={searchTerm}
+          placeholder="Search (ex. landscape)"
+          onChangeText={text => setSearchTerm(text)}
+        />
+        {loadingIndicator}
+      </View>
+    );
+
+    const emptyState = !loading && (
+      <View style={styles.container}>
+        <Text>No results</Text>
+      </View>
+    );
+
     return (
       <FlatList
+        ListHeaderComponent={searchField}
+        ListEmptyComponent={emptyState}
         numColumns={3}
         renderItem={renderArtItem}
         data={artList}
-      ></FlatList>
+        refreshing={loading}
+      />
     );
   };
 
-  return renderArtList();
+  return (
+    <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+      {renderArtList()}
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-  loadingContainer: {
+  container: {
     flex: 1,
     flexDirection: 'column',
-    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
